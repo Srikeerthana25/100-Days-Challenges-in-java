df8231d1177606ce38a8ca6259a89e5e54a23bdb
Srikeerthana25 <109751615+Srikeerthana25@users.noreply.github.com>
2024-11-11 21:01:55 +0530

    Day-1

---
+++ CheckAlphabet.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CheckAlphabet{
+ public static void main(String[] args){
+
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter a character:");
+  char val = scan.next().charAt(0);
+ 
+  if ((val >= 'a' && val <='z') || (val >= 'A' && val <='Z') ){
+    System.out.println( val + "is alphabet");
+}
+ else{
+ System.out.println( val + "Is not Alphabet");
+}
+ 
+ }
+}
